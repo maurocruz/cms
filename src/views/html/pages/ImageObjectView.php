d7ab33509fecdd3052c5c1a99f2c094ab2f27c07
Mauro Cruz <maurocruz@pirenopolis.tur.br>
2020-07-28 14:42:23 -0300

    Improved forms

--- src/views/html/pages/ImageObjectView.php
+++ src/views/html/pages/ImageObjectView.php
@@ -7,15 +7,15 @@ use Plinct\Api\Type\PropertyValue;
 
 class ImageObjectView
 {
-    protected $tableOwner;
-    protected $idOwner;
+    protected $tableHasPart;
+    protected $idHasPart;
     
     use \Plinct\Web\Widget\FormTrait;    
             
-    public function getForm($tableOwner, $idOwner, $data = []) 
+    public function getForm($tableHasPart, $idHasPart, $data = []) 
     {
-        $this->tableOwner = $tableOwner;
-        $this->idOwner = $idOwner;
+        $this->tableHasPart = $tableHasPart;
+        $this->idHasPart = $idHasPart;
         // form for edit
         $content[] = self::edit($data ?? []);
         // upload
@@ -37,9 +37,13 @@ class ImageObjectView
     {
         if (empty($data)) {
             $content[] = [ "tag" => "p", "content" => "Não há imagens!", "attributes" => [ "class" => "aviso"] ];
+            
         } else {
             foreach ($data as $valueEdit) {
-                $content[] = self::form($valueEdit, $data['isPartOf'] ?? null);
+                $content[] = self::simpleTag("div", [
+                    self::form($valueEdit, $data['isPartOf'] ?? null),
+                    self::formIsPartOf($valueEdit)
+                ], [ "class" => "box "]);
             }              
         }        
         return $content;
@@ -48,6 +52,7 @@ class ImageObjectView
     private function form($value, $isPartOf = null, $mode = "simple") 
     { 
         $ID = PropertyValue::extractValue($value['identifier'], "id");
+        
         if (isset($value['potentialAction'])) {
             foreach ($value['potentialAction'] as $valueAction) {
                 $potentialAction[$valueAction['name']] = $valueAction['result'];
@@ -56,18 +61,34 @@ class ImageObjectView
         
         $content[] = isset($isPartOf) && $isPartOf['@type'] == "WebPage" ? [ "tag" => "input", "attributes" => [ "name" => "idwebPage", "type" => "hidden", "value" => $isPartOf['identifier'] ] ] : null;
         
-        $content[] = [ "tag" => "input", "attributes" => [ "name" => "tableOwner", "type" => "hidden", "value" => $this->tableOwner ] ];
-        $content[] = [ "tag" => "input", "attributes" => [ "name" => "idOwner", "type" => "hidden", "value" => $this->idOwner ] ];
-        $content[] = [ "tag" => "input", "attributes" => [ "name" => "idimageObject", "type" => "hidden", "value" => $ID ] ];
+        $content[] = [ "tag" => "input", "attributes" => [ "name" => "id", "type" => "hidden", "value" => $ID ] ];
         
         // figure
         $content[] = [ "object" => "figure", "attributes" => [ "style" => "max-width: 200px; float: left; margin-right: 10px;" ], "src" => $value['contentUrl'], "width" => 200 ];
+        
         // ID
         $content[] = [ "tag" => "p", "content" => "[ID=$ID] ".$value['contentUrl'] ];
+        
         // group
-        $content[] = self::fieldsetWithInput(_("Keywords"), "keywords", $value['keywords'], [ "style" => "width: 240px;" ]);
+        $content[] = self::fieldsetWithInput(_("Keywords"), "keywords", $value['keywords'], [ "style" => "width: calc(100% - 280px);" ]);        
+        
+        $content[] = self::submitButtonSend();
+        //$content[] = self::submitButtonDelete("/admin/imageObject/erase");
+        // form
+        return [ "tag" => "form", "attributes" => [ "class" => "formPadrao", "style" => "overflow: hidden;", "id" => "form-images-edit-{$ID}", "name" => "form-images-edit", "action" => "/admin/imageObject/edit", "enctype" => "multipart/form-data", "method" => "post" ], "content" => $content ];
+    }
+        
+    private function formIsPartOf($value, $mode = 'simple') 
+    {       
+        $ID = PropertyValue::extractValue($value['identifier'], "id");
+        
+        $content[] = [ "tag" => "input", "attributes" => [ "name" => "tableHasPart", "type" => "hidden", "value" => $this->tableHasPart ] ];
+        $content[] = [ "tag" => "input", "attributes" => [ "name" => "idHasPart", "type" => "hidden", "value" => $this->idHasPart ] ];
+        $content[] = [ "tag" => "input", "attributes" => [ "name" => "idIsPartOf", "type" => "hidden", "value" => $ID ] ];
+        
         // position
         $content[] = self::fieldsetWithInput(_("Position"), "position", $value['position'], [ "style" => "width: 80px;" ], "number", [ "min" => "1" ]);
+        
         // highlights
         $content[] = [ "tag" => "fieldset", "attributes" => [ "style" => "min-width: 125px; margin: 5px 0;" ], "content" => [
             [ "tag" => "legend", "content" => _("Representative of page") ],
@@ -84,7 +105,9 @@ class ImageObjectView
         $content[] = [ "tag" => "fieldset", "attributes" => [ "style" => "width: 700px; margin 5px 0;" ], "content" => [
             [ "tag" => "legend", "content" => "Legenda" ],
             [ "tag" => "input", "attributes" => [ "name" => "caption", "type" => "text", "value" => $value['caption'] ?? null ] ]
-        ]]; 
+        ]];
+        
+        
         if ($mode == "complete") {
             $content[] = "<br>";
             // width
@@ -116,39 +139,39 @@ class ImageObjectView
             ]];
         }
         //
-        $content[] = self::submitButtonSend([ "onclick" => "return submitFromAjax(this,'form-images-edit-$ID');" ]);
-        $content[] = self::submitButtonDelete("/admin/imageObject/deleteRelationship");
+        $content[] = self::submitButtonSend();
+        $content[] = self::submitButtonDelete("/admin/imageObject/erase");
         // form
-        return [ "tag" => "form", "attributes" => [ "class" => "box formPadrao", "style" => "overflow: hidden;", "id" => "form-images-edit-{$ID}", "name" => "form-images-edit", "action" => "/admin/imageObject/edit", "enctype" => "multipart/form-data", "method" => "post" ], "content" => $content ];
+        return [ "tag" => "form", "attributes" => [ "class" => "formPadrao", "style" => "overflow: hidden;", "id" => "form-images-edit-{$ID}", "name" => "form-images-edit", "action" => "/admin/imageObject/edit", "enctype" => "multipart/form-data", "method" => "post" ], "content" => $content ];
     }
         
     protected function addImagesFromDatabase() 
     {        
-        $content[] = [ "tag" => "input", "attributes" => [ "name" => "tableOwner", "type" => "hidden", "value" => $this->tableOwner ] ];
-        $content[] = [ "tag" => "input", "attributes" => [ "name" => "idOwner", "type" => "hidden", "value" => $this->idOwner ] ];
+        $content[] = [ "tag" => "input", "attributes" => [ "name" => "tableHasPart", "type" => "hidden", "value" => $this->tableHasPart ] ];
+        $content[] = [ "tag" => "input", "attributes" => [ "name" => "idHasPart", "type" => "hidden", "value" => $this->idHasPart ] ];
         
         $content[] = [ "tag" => "div", "attributes" => [ "id" => "imagesfromdatabase", "class" => "imagesFromDatabase" ] ];
         
-        return [ "tag" => "form", "attributes" => [ "action" => "/admin/imageObject/postRelationship", "name" => "imagesFromDatabase", "class" => "formPadrao box", "method" => "post" ], "content" => $content ];
+        return [ "tag" => "form", "attributes" => [ "action" => "/admin/imageObject/new", "name" => "imagesFromDatabase", "class" => "formPadrao box", "method" => "post" ], "content" => $content ];
     }
     
     protected function addImagesFromServer($idwebPage = null) 
     {        
-        $content[] = [ "tag" => "input", "attributes" => [ "name" => "tableOwner", "type" => "hidden", "value" => $this->tableOwner ] ];
-        $content[] = [ "tag" => "input", "attributes" => [ "name" => "idOwner", "type" => "hidden", "value" => $this->idOwner ] ];
+        $content[] = [ "tag" => "input", "attributes" => [ "name" => "tableHasPart", "type" => "hidden", "value" => $this->tableHasPart ] ];
+        $content[] = [ "tag" => "input", "attributes" => [ "name" => "idHasPart", "type" => "hidden", "value" => $this->idHasPart ] ];
         $content[] = $idwebPage ? [ "tag" => "input", "attributes" => [ "name" => "idwebPage", "type" => "hidden", "value" => $idwebPage ] ] : null;
         
         $content[] = [ "tag" => "div", "attributes" => [ "id" => "imagesfromserver" ] ];        
         
-        return [ "tag" => "form", "attributes" => [ "action" => "/admin/imageObject/insertHasPartFromServer", "name" => "images-selectedFromServer", "id" => "images-selectedFromServer-".$this->idOwner, "class" => "formPadrao box", "enctype" => "multipart/form-data", "method" => "post" ], "content" => $content ];
+        return [ "tag" => "form", "attributes" => [ "action" => "/admin/imageObject/insertHasPartFromServer", "name" => "images-selectedFromServer", "id" => "images-selectedFromServer-".$this->idHasPart, "class" => "formPadrao box", "enctype" => "multipart/form-data", "method" => "post" ], "content" => $content ];
     }
     
     protected function upload($isPartOf = null) 
     {
         $content[] = [ "tag" => "h4", "content" => "Enviar imagem" ];
         $content[] = $isPartOf ? [ "tag" => "input", "attributes" => [ "name" => "idwebPage", "value" => $isPartOf['identifier'], "type" => "hidden" ] ] : null;
-        $content[] = [ "tag" => "input", "attributes" => [ "name" => "tableOwner", "value" => $this->tableOwner, "type" => "hidden" ] ];
-        $content[] = [ "tag" => "input", "attributes" => [ "name" => "idOwner", "value" => $this->idOwner, "type" => "hidden" ] ];
+        $content[] = [ "tag" => "input", "attributes" => [ "name" => "tableHasPart", "value" => $this->tableHasPart, "type" => "hidden" ] ];
+        $content[] = [ "tag" => "input", "attributes" => [ "name" => "idHasPart", "value" => $this->idHasPart, "type" => "hidden" ] ];
         $content[] = [ "tag" => "fieldset", "content" => [
             [ "tag" => "legend", "content" => "Enviar imagem" ],
             [ "tag" => "input", "attributes" => [ "name" => "imageupload", "type" => "file"] ]
@@ -163,6 +186,6 @@ class ImageObjectView
         ] ];
         $content[] = self::submitButtonSend();
         
-        return [ "tag" => "form", "attributes" => [ "name" => "form-images-upload", "id" => "form-images-uploadImage-".$this->idOwner, "action" => '/admin/imageObject/newAndPostRelationship', "class" => "box formPadrao", "enctype" => "multipart/form-data", "method" => "post" ], "content" => $content ]; 
+        return [ "tag" => "form", "attributes" => [ "name" => "form-images-upload", "id" => "form-images-uploadImage-".$this->idHasPart, "action" => '/admin/imageObject/newAndPostRelationship', "class" => "box formPadrao", "enctype" => "multipart/form-data", "method" => "post" ], "content" => $content ]; 
     }
 }
