2c5ca614e248719dccbef3b517eb64e03948b2bf
Mauro Cruz <maurocruz@pirenopolis.tur.br>
2020-07-29 14:02:10 -0300

    Eliminated function deprecated

--- src/controllers/PersonController.php
+++ src/controllers/PersonController.php
@@ -26,25 +26,4 @@ class PersonController implements ControllerInterface
         
         return (new Person())->get($params);
     }
-    
-    public function getContent() 
-    {
-        switch ($this->action) 
-        {
-            case 'edit':
-                $get = new \fwc\Thing\PersonGet();
-                $get->setProperties("givenName", "familyName", "additionalName", "taxId", "birthDate", "birthPlace", "gender", "hasOccupation", "contactPoint", "address");
-                $get->setTypes("PostalAddress", "ContactPoint");
-                $data = $get->selectById($this->identifier, null, "*", true); 
-                //var_dump(json_decode($data));                
-                return $data;
-                
-            default:
-                $where = filter_input(INPUT_GET, 'search') ? "`name` LIKE '%$search%'" : null;
-                $limit = 200;
-                $order = "dateModified DESC, givenName ASC"; 
-                
-                return (new \fwc\Thing\PersonGet())->listAll($where, $order, $limit);
-        }
-    }
 }
