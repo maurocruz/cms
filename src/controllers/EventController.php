80f6ddce67b21abd356b09b4a9d9e696ceb84fb0
Mauro Cruz <maurocruz@pirenopolis.tur.br>
2020-08-25 14:19:39 -0300

    Added to git

---
+++ src/controllers/EventController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Plinct\Cms\Controller;
+
+use Plinct\Api\Type\Event;
+use Plinct\Cms\App;
+use Plinct\Tool\Sitemap;
+
+class EventController implements ControllerInterface
+{
+    public function index($params = null): array
+    {
+        $params2 = array_merge([ "format" => "ItemList", "orderBy" => "startDate", "ordering" => "desc" ], $params);
+
+        return (new Event())->get($params2);
+    }
+
+    public function new()
+    {
+        return true;
+    }
+
+    public function edit(array $params): array
+    {
+        $params= array_merge($params, [ "properties" => "*,location,image" ]);
+
+        return (new Event())->get($params);
+    }
+
+    public function saveSitemap()
+    {
+        $params = [ "orderBy" => "startDate", "ordering" => "desc", "limit" => "none" ];
+        $data = (new Event())->get($params);
+
+        foreach ($data as $value) {
+            $dataSitemap[] = [
+                "loc" => "http://" . filter_input(INPUT_SERVER, 'HTTP_HOST')
+                . DIRECTORY_SEPARATOR . "eventos"
+                . DIRECTORY_SEPARATOR . substr($value['startDate'],0,10)
+                . DIRECTORY_SEPARATOR . urlencode($value['name']),
+                "name" => App::getTitle(),
+                "language" => App::getLanguage(),
+                "publication_date" => $value['startDate'],
+                "title" => $value['name']
+            ];
+        }
+
+        (new Sitemap("sitemap-event.xml"))->createSitemapNews($dataSitemap);
+    }
+}
