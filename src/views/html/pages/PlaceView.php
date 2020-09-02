ab018232a549b4a63b488df11cc1818ccc604150
Mauro Cruz <maurocruz@pirenopolis.tur.br>
2020-09-02 16:46:52 -0300

    enable if condition for auto search for new when is part of

--- src/views/html/pages/PlaceView.php
+++ src/views/html/pages/PlaceView.php
@@ -3,15 +3,19 @@
 namespace Plinct\Cms\View\Html\Page;
 
 use Plinct\Api\Type\PropertyValue;
+use Plinct\Cms\View\Html\Piece\navbarTrait;
+use Plinct\Web\Widget\FormTrait;
 
 class PlaceView
 {
+    private $content;
+
     protected $placeId;
     
     protected $placeName;
 
-    use \Plinct\Cms\View\Html\Piece\navbarTrait;
-    use \Plinct\Web\Widget\FormTrait;
+    use navbarTrait;
+    use FormTrait;
     
     public function navbar() 
     {
@@ -34,7 +38,7 @@ class PlaceView
     public function new($data = null): array
     {
         $this->navbar();
-        
+
         $this->content['main'][] = self::divBox(_("Add new"), "Place", [ self::form(null, null) ]);
         
         return $this->content;
@@ -75,37 +79,39 @@ class PlaceView
     private function form($tableHasPart, $idHasPart, $case = "new", $value = null) 
     {
         $content[] = $tableHasPart ? [ "tag" => "input", "attributes" => [ "name" => "tableHasPart", "type" => "hidden", "value" => $tableHasPart ]] : null;        
-        $content[] = $idHasPart ? [ "tag" => "input", "attributes" => [ "name" => "idHasPart", "type" => "hidden", "value" => $idHasPart ]] :  null; 
-        
-        if ($case == "edit" ) {
-            $content[] = [ "tag" => "input", "attributes" => [ "name" => "id", "type" => "hidden", "value" => $this->placeId ] ];
-        }   
-        
-        // name        
-        $content[] = $case == "addWithPart" ? self::searchAndSubmit("name", "place", "name", $value['location'] ?? $value) : self::fieldsetWithInput(_("Place"), "name", $value['name'], [ "style" => "width: 320px;"]);        
-        // Geo
-        $content[] = self::fieldsetWithInput(_("Latitude"), "latitude", $value['latitude'], [ "style" => "width: 225px;"]);
-        
-        $content[] = self::fieldsetWithInput(_("Longitude"), "longitude", $value['longitude'], [ "style" => "width: 225px;"]);
-        
-        // elevation
-        $content[] = self::fieldsetWithInput(_("Elevation (meters)"), "elevation", $value['elevation'], [ "style" => "width: 200px;"]);
-        
-        // additional type
-        $content[] = self::fieldsetWithInput(_("Additional type"), "additionalType", $value['additionalType'], [ "style" => "width: 500px;"]);
-        
-        // description
-        //$content[] = self::fieldsetWithTextarea(_("Description"), "description", $value['description'], 100);
-        
-        // disambiguating description
-        //$content[] = self::fieldsetWithTextarea(_("Disambiguating description"), "disambiguatingDescription", $value['disambiguatingDescription'], 150);
-        
-        // submit
-        $content[] = self::submitButtonSend();
-        
-        if ($case == "edit") {
-            $action = $tableHasPart ? "/admin/place/deleteRelationship" : "/admin/place/erase";
-            $content[] = self::submitButtonDelete($action);
+        $content[] = $idHasPart ? [ "tag" => "input", "attributes" => [ "name" => "idHasPart", "type" => "hidden", "value" => $idHasPart ]] :  null;
+
+        $content[] = $case == "edit" ? ["tag" => "input", "attributes" => ["name" => "id", "type" => "hidden", "value" => $this->placeId]]: null;
+
+        if ($case == "new" && $tableHasPart && $idHasPart) {
+            $content[] = [ "tag" => "div", "attributes" => [ "class" => "add-existent", "data-type" => "place" ] ];
+        } else {
+            // name
+            $content[] = self::fieldsetWithInput(_("Place"), "name", $value['name'], ["style" => "width: 320px;"]);
+            // Geo
+            $content[] = self::fieldsetWithInput(_("Latitude"), "latitude", $value['latitude'], ["style" => "width: 225px;"]);
+
+            $content[] = self::fieldsetWithInput(_("Longitude"), "longitude", $value['longitude'], ["style" => "width: 225px;"]);
+
+            // elevation
+            $content[] = self::fieldsetWithInput(_("Elevation (meters)"), "elevation", $value['elevation'], ["style" => "width: 200px;"]);
+
+            // additional type
+            $content[] = self::fieldsetWithInput(_("Additional type"), "additionalType", $value['additionalType'], ["style" => "width: 500px;"]);
+
+            // description
+            $content[] = self::fieldsetWithTextarea(_("Description"), "description", $value['description'], 100);
+
+            // disambiguating description
+            $content[] = self::fieldsetWithTextarea(_("Disambiguating description"), "disambiguatingDescription", $value['disambiguatingDescription'], 150);
+
+            // submit
+            $content[] = self::submitButtonSend();
+
+            if ($case == "edit") {
+                $action = $tableHasPart ? "/admin/place/deleteRelationship" : "/admin/place/erase";
+                $content[] = self::submitButtonDelete($action);
+            }
         }
         
         return  [ "tag" => "form", "attributes" => [ "id" => "form-place-$case", "name" => "place-form-".$case, "class" => "formPadrao", "method" => "post", "action" => "/admin/place/".$case ], "content" => $content ];
