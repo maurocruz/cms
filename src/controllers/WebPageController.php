20062ec4d30c93ef31f9760d3003589d35eb386d
Mauro Cruz <maurocruz@pirenopolis.tur.br>
2020-10-01 15:27:48 -0300

    Added saveSiteMap function

--- src/controllers/WebPageController.php
+++ src/controllers/WebPageController.php
@@ -3,6 +3,7 @@
 namespace Plinct\Cms\Controller;
 
 use Plinct\Api\Type\WebPage;
+use Plinct\Tool\Sitemap;
 
 class WebPageController implements ControllerInterface
 {   
@@ -23,5 +24,20 @@ class WebPageController implements ControllerInterface
     public function new() 
     {
         return true;
+    }
+
+    public function saveSitemap()
+    {
+        $dataSitemap = null;
+
+        $data = (new WebPage())->get([]);
+
+        foreach ($data as $value) {
+            $dataSitemap[] = [
+                "loc" => "http://" . filter_input(INPUT_SERVER, 'HTTP_HOST').$value['url'],
+            ];
+        }
+
+        (new Sitemap("sitemap.xml"))->createSitemap($dataSitemap);
     }
 }
