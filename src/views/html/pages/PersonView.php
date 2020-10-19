82651427f50d7af8c488a2fb8f4304b9b9db6cbe
Mauro Cruz <maurocruz@pirenopolis.tur.br>
2020-10-19 11:02:03 -0300

    adding new navbar with search form

--- src/views/html/pages/PersonView.php
+++ src/views/html/pages/PersonView.php
@@ -3,26 +3,35 @@
 namespace Plinct\Cms\View\Html\Page;
 
 use Plinct\Api\Type\PropertyValue;
+use Plinct\Cms\View\Html\Piece\navbarTrait;
+use Plinct\Cms\Views\Html\Piece\FormTrait;
 
 class PersonView
 {
     protected $content;
         
-    use \Plinct\Cms\View\Html\Piece\navbarTrait;
-    use \Plinct\Web\Widget\FormTrait;
+    use navbarTrait;
+    use FormTrait;
 
-    public function navbar($id = null, $name = null) 
+    public function navbarPerson($id = null, $name = null)
     {
-        $this->content['navbar'][] = $this->personNavbar();
+        $title = _("Person");
+        $list = [
+            "/admin/person" => _("View all"),
+            "/admin/person/new" => _("Add new person")
+        ];
+        $search = [ "tag" => "div", "attributes" => [ "class" => "navbar-search", "data-type" => "person", "data-searchfor" => "name" ] ];
+
+        $this->content['navbar'][] = self::navbar($title, $list, 2, $search);
         
         if($id) {
-            $this->content['navbar'][] = $this->personNavbar($id, $name, null, 3);
+            $this->content['navbar'][] = self::navbar($name, [], 3);
         }
     }
     
     public function index(array $data): array
     {
-        $this->navbar();
+        $this->navbarPerson();
         
         $this->content['main'][] = self::listAll($data, "person", _("List of persons"), [ "dateModified" => "Date modified" ]);
         
@@ -31,7 +40,7 @@ class PersonView
     
     public function new($data = null): array
     {
-        $this->navbar();
+        $this->navbarPerson();
         
         $this->content['main'][] = [ "tag" => "h3", "content" => _("Add new person") ];
         $this->content['main'][] = self::form();
@@ -45,7 +54,7 @@ class PersonView
         
         $id = PropertyValue::extractValue($value['identifier'], "id");
         
-        $this->navbar($id, $value['name']);
+        $this->navbarPerson($id, $value['name']);
         
         // form
         $this->content['main'][] = self::form('edit', $value);
@@ -57,19 +66,6 @@ class PersonView
         $this->content['main'][] = self::divBoxExpanding(_("Postal address"), "PostalAddres", [ (new PostalAddressView())->getForm("person", $id, $value['address']) ]);
         
         return $this->content;
-    }
-    
-    public function getForm($tableHasPart, $idHasPart, $data) 
-    {        
-        if ($data) {
-            // show persons
-            foreach ($data as $value) {
-                $content[] = self::form('edit', $value, $tableHasPart, $idHasPart);
-            }
-        }        
-        // insert new person
-        $content[] = self::form("new", null, $tableHasPart, $idHasPart);        
-        return $content;
     }
     
     private static function form($case = 'new', $value = null, $tableHasPart = null, $idHasPart = null)
@@ -145,34 +141,5 @@ class PersonView
         }
         
         return [ "tag" => "form", "attributes" => [ "class" => "form-inline box", "action" => "/admin/person/$case", "method" => "post"], "content" => $content ];
-    }
-    
-    private static function formWithPartOf($value, $tableHasPart, $idHasPart) 
-    {
-        $id = PropertyValue::extractValue($value['identifier'], "id");
-        
-        $content[] = [ "tag" => "h3", "content" => ucfirst(_("Person")) ]; 
-        
-        $content[] = [ "tag" => "input", "attributes" => [ "name"=>"id", "type" => "hidden", "value" => $id ] ];
-        $content[] = [ "tag" => "input", "attributes" => [ "name"=>"tableHasPart", "type" => "hidden", "value" => $tableHasPart ] ] ;
-        $content[] = [ "tag" => "input", "attributes" => [ "name"=>"idHasPart", "type" => "hidden", "value" => $idHasPart ] ];
-        
-        // given name
-        $content[] = [ "tag" => "fieldset", "attributes" => [ "style" => "width: 64%;" ], "content" => [ 
-            [ "tag" =>"legend", "content" => _("Name")." - "._("taxId")." [<a href=\"/admin/modules/person/edit/".$id."\" class=\"white\">"._("Edit person")."</a>]" ],
-            [ "tag" => "input", "attributes" => [ "name"=>"listPersonNames", "type" => "text", "value" => $value['name'], "onKeyUp" => "selectItemFormBd(this,'person');", "autocomplete" => "off", "readonly" ] ]
-        ]];
-        
-        // jobTitle
-        $content[] = [ "tag" => "fieldset", "attributes" => [ "style" => "width: 23%;" ], "content" => [ 
-                [ "tag" =>"legend", "content" => _("Job title") ],
-                [ "tag" => "input", "attributes" => [ "name"=>"jobTitle", "type" => "text", "value" => $value['jobTitle'] ?? null ] ]
-            ]]; 
-        
-        $content[] = self::submitButtonSend();
-        
-        $content[] = self::submitButtonDelete("/admin/person/erase");
-        
-        return [ "tag" => "form", "attributes" => [ "class" => "form-inline box", "action" => "/admin/person/edit", "method" => "post" ], "content" => $content ];
     }
 }
