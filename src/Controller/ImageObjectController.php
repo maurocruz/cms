4fde7404127d444c4b10e83d9ffa22a51f861be2
Mauro Cruz <maurocruz@pirenopolis.tur.br>
2020-11-12 17:07:27 -0300

    changed many things for update image object module view

--- src/Controller/ImageObjectController.php
+++ src/Controller/ImageObjectController.php
@@ -2,14 +2,15 @@
 
 namespace Plinct\Cms\Controller;
 
-use Plinct\Api\Server\PDOConnect;
 use Plinct\Api\Type\ImageObject;
+use Plinct\Api\Type\PropertyValue;
+use Plinct\Cms\Server\ImageObjectServer;
 
 class ImageObjectController implements ControllerInterface
 {
     public function index($params = null): array
     {
-        $params2 = [ "format" => "ItemList", "groupBy" => "keywords", "limit" => "none", "orderBy" => "uploadDate desc, name" ];
+        $params2 = [ "format" => "ItemList", "groupBy" => "keywords", "limit" => "none", "orderBy" => "keywords" ];
 
         $params3 = $params ? array_merge($params2, $params) : $params2;
 
@@ -22,16 +23,29 @@ class ImageObjectController implements ControllerInterface
 
         $params2 = [ "format" => "ItemList", "keywords" => $keywords, "limit" => "none", "orderBy" => "uploadDate desc, name" ];
 
-        return(new ImageObject())->get($params2);
+        $data['list'] = (new ImageObject())->get($params2);
+        $data['paramsUrl'] = $params;
+
+        return $data;
     }
 
     public function new()
     {
-        // TODO: Implement new() method.
+        return null;
     }
 
     public function edit(array $params): array
     {
-        // TODO: Implement edit() method.
+        $params2 = [ "properties" => "*,author" ];
+
+        $params3 = array_merge($params2, $params);
+
+        $data = (new ImageObject())->get($params3);
+
+        $value = $data[0];
+        $id = PropertyValue::extractValue($value['identifier'], "id");
+        $value['info'] = (new ImageObjectServer())->getImageHasPartOf($id);
+
+        return $value;
     }
 }
--- src/Server/ImageObjectServer.php
+++ src/Server/ImageObjectServer.php
@@ -3,18 +3,20 @@
 namespace Plinct\Cms\Server;
 
 use Plinct\Api\Server\PDOConnect;
-use Plinct\Cms\App;
+use Plinct\Api\Type\ImageObject;
 
 class ImageObjectServer
 {
     private $tablesHasImageObject;
+    private static $KEYWORDS_LIST;
+    private static $KEYWORDS;
+    private static $LIST_LOCATIONS;
 
     public function __construct()
     {
         $table_schema = PDOConnect::getDbname();
         $this->tablesHasImageObject = PDOConnect::run("select table_name from information_schema.tables WHERE table_schema = '$table_schema' AND  table_name LIKE '%_has_imageObject';");
     }
-
 
     public function getImageHasPartOf($idIsPartOf)
     {
@@ -37,5 +39,40 @@ class ImageObjectServer
         }
 
         return $info;
+    }
+
+    public static function listLocation($directory)
+    {
+        self::$LIST_LOCATIONS = self::$LIST_LOCATIONS ?? self::listDirectories($directory);
+
+        return self::$LIST_LOCATIONS;
+    }
+
+    private static function listDirectories($directory, $arrayReturn = null)
+    {
+        foreach (scandir($directory) as $key => $value) {
+            if (!in_array($value,array(".","..","thumbs"))) {
+                $path = $directory . DIRECTORY_SEPARATOR . $value;
+                if (is_dir($directory . DIRECTORY_SEPARATOR . $value)) {
+                    $arrayReturn[] = str_replace($_SERVER['DOCUMENT_ROOT'], "",$path);
+                    $arrayReturn = self::listDirectories($path, $arrayReturn);
+                }
+            }
+        }
+
+        return $arrayReturn;
+    }
+
+    public static function listKeywords()
+    {
+        self::$KEYWORDS_LIST = self::$KEYWORDS_LIST ?? (new ImageObject())->get([ "groupBy" => "keywords", "orderBy" => "keywords" ]);
+
+        if(self::$KEYWORDS === null) {
+            foreach (self::$KEYWORDS_LIST as $value) {
+                self::$KEYWORDS[] = $value['keywords'];
+            }
+        }
+
+        return self::$KEYWORDS;
     }
 }
