cf35328e95451ebe304b651f80550ea9d469c053
Mauro Cruz <maurocruz@pirenopolis.tur.br>
2021-01-28 12:23:43 -0300

    included functions payment and expired

--- src/Controller/OrderController.php
+++ src/Controller/OrderController.php
@@ -2,29 +2,40 @@
 
 namespace Plinct\Cms\Controller;
 
+use Plinct\Api\Server\PDOConnect;
+use Plinct\Api\Type\Banner;
 use Plinct\Api\Type\Order;
 
 class OrderController implements ControllerInterface
 {
-
     public function index($params = null): array
     {
-        $search = $params['search'] ?? null;
+        $nameLike = $params['search'] ?? null;
 
-        $paramsSearch = $search ? [ "nameLike" => $search ] : [];
+        $params2 = [ "format" => "ItemList", "properties" => "*,seller,customer,orderedItem", "orderBy" => "orderStatus='orderProcessing' DESC, orderDate DESC", "limit" => "200" ];
 
-        $params2 = [ "format" => "ItemList", "properties" => "*", "orderBy" => "paymentDueDate DESC, orderStatus DESC", "limit" => "none" ];
+        if($nameLike) {
+            return (new Order())->search($params2, $nameLike);
+        }
 
-        $params3 = array_merge($params2, $paramsSearch);
-
-        return (new Order())->get($params3);
+        return (new Order())->get($params2);
     }
 
     public function edit(array $params): array
     {
         $params2 = [ "id" => $params['id'], "properties" => "*,customer,seller,orderedItem,partOfInvoice,history" ];
 
-        return $data = (new Order())->get($params2);
+        $data = (new Order())->get($params2);
+
+        // banner
+        if ($data[0]['tipo'] == '4') {
+            $idorder = $data[0]['idorder'];
+            $paramsBanner = [ "where" => "`idorder`=$idorder" ];
+            $bannerData = (new Banner())->get($paramsBanner);
+            $data['banner'] = $bannerData[0] ?? null;
+        }
+
+        return $data;
     }
 
     public function new($params = null): ?array
@@ -39,5 +50,41 @@ class OrderController implements ControllerInterface
             return $data;
         }
         return null;
+    }
+
+    public function payment(): array
+    {
+        $date = self::translatePeriod(filter_input(INPUT_GET, 'period'));
+
+        $query = "SELECT `invoice`.paymentDueDate, `localBusiness`.name, `invoice`.totalPaymentDue, `order`.idorder, `contratostipos`.contrato_name, `order`.orderStatus, (SELECT COUNT(*) FROM `invoice` WHERE `invoice`.referencesOrder=`order`.idorder) as number_parc FROM `invoice`, `order`, localBusiness, contratostipos WHERE (`invoice`.paymentDate = '0000-00-00' OR `invoice`.paymentDate is null) AND `invoice`.referencesOrder=`order`.idorder and `order`.orderStatus!='' AND `order`.customer=localBusiness.idlocalBusiness AND `order`.tipo=contratostipos.idcontratostipo";
+        $query .= $date ? " AND `invoice`.paymentDueDate <= '$date'" : null;
+        $query .= " ORDER BY `invoice`.paymentDueDate ASC;";
+
+        return PDOConnect::run($query);
+    }
+
+    public function expired(): array
+    {
+        $dateLimit = self::translatePeriod(filter_input(INPUT_GET, 'period'));
+
+        $query = "SELECT `order`.*, localBusiness.name, contratostipos.contrato_name FROM `order`, contratostipos, localBusiness WHERE `order`.orderStatus='orderProcessing' AND contratostipos.idcontratostipo=`order`.tipo AND `order`.customer=localBusiness.idlocalBusiness";
+        $query .= $dateLimit ? " AND `order`.paymentDueDate < '$dateLimit'" : null;
+        $query .= " ORDER BY `order`.paymentDueDate ASC;";
+
+        return PDOConnect::run($query);
+    }
+
+    static private function translatePeriod($get)
+    {
+        switch ($get) {
+            case "past":
+                return date("Y-m-d");
+
+            case "current_month":
+                return date('Y-m-t');
+
+            default:
+                return null;
+        }
     }
 }
