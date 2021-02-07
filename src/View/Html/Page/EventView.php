2f749080bdf271ff49c6f0fe7797846fece9da3f
Mauro Cruz <maurocruz@pirenopolis.tur.br>
2021-02-07 12:48:17 -0300

    ordered item and delete empty lines

--- src/View/Html/Page/EventView.php
+++ src/View/Html/Page/EventView.php
@@ -16,66 +16,50 @@ class EventView
     protected function navbarEvent()
     {
         $title = _("Events");
-        $list = [
-            "/admin/event" => _("View all"),
-            "/admin/event/new" => _("Add new")
-        ];
+        $list = [ "/admin/event" => _("View all"), "/admin/event/new" => _("Add new") ];
         $level = 3;
         $appendNavbar = [ "tag" => "div", "attributes" => [ "class" => "navbar-search", "data-type" => "event", "data-searchfor" => "name" ] ];
-
         $this->content['navbar'][] = self::navbar($title, $list, $level, $appendNavbar);
     }
     
     public function index(array $data): array 
     {
         $this->navbarEvent();
-        
         $this->content['main'][] = self::listAll($data, "event", _("List of events"), [ "startDate" => _("Date") ]);
-        
         return $this->content;
     }
         
     public function new(): array
     {
         $this->navbarEvent();
-        
-        $this->content['main'][] = self::div(_("Add new"), "event", [ self::form() ]);
-        
+        $this->content['main'][] = self::div(_("Add new"), "event", [ self::formEvent() ]);
         return $this->content;
     }
         
     public function edit(array $data): array 
     {            
         $this->navbarEvent();
-        
         if (!$data) {
             $content[] = [ "tag" => "p", "attributes" => [ "class" => "aviso" ], "content" => _("Event not found") ];            
         } else {
             $value = $data[0];
-
             $idEvent = PropertyValue::extractValue($value['identifier'], 'id');
-
             $content[] = [ "tag" => "p", "content" => _("View on website"), "href" => "/eventos/". substr($value['startDate'], 0, 10)."/". urlencode($value['name']), "hrefAttributes" => [ "target" => "_blank" ] ];
-
             // event 
-            $content[] = self::form('edit', $value);
-
+            $content[] = self::formEvent('edit', $value);
             // place
             $content[] = self::divBoxExpanding(_("Place"), "place", [ self::relationshipOneToOne("event", $idEvent, "location", "place", $value['location']) ]);
-
             // images
             $content[] = self::divBoxExpanding(_("Images"), "ImageObject", [ (new ImageObjectView())->getForm("event", $idEvent, $value['image']) ]);
         }
-
         $this->content['main'][] = [ "tag" => "div", "attributes" => [ "class" => "events" ], "content" => $content ];
-        
         return $this->content;
     }
 
     /*
      * Formulário de edição dos dados do evento
      */
-    private static function form($case = 'new', $value = null)
+    private static function formEvent($case = 'new', $value = null): array
     {
         $startDate = strstr($value['startDate'], " ", true); 
         $startTime = substr(strstr($value['startDate'], " "),1);
