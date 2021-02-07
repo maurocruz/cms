1758fdc33551226ccec91be7ee3091a961054977
Mauro Cruz <maurocruz@pirenopolis.tur.br>
2021-02-07 12:49:43 -0300

    ordered item and delete empty lines

--- src/View/Html/Page/PersonView.php
+++ src/View/Html/Page/PersonView.php
@@ -13,102 +13,74 @@ class PersonView
     use navbarTrait;
     use FormElementsTrait;
 
-    public function navbarPerson($id = null, $name = null)
-    {
+    public function navbarPerson($id = null, $name = null) {
         $title = _("Person");
-        $list = [
-            "/admin/person" => _("View all"),
-            "/admin/person/new" => _("Add new person")
-        ];
+        $list = [ "/admin/person" => _("View all"), "/admin/person/new" => _("Add new person") ];
         $search = self::searchPopupList("person");
-
         $this->content['navbar'][] = self::navbar($title, $list, 2, $search);
-        
         if($id) {
             $this->content['navbar'][] = self::navbar($name, [], 3);
         }
     }
     
-    public function index(array $data): array
-    {
+    public function index(array $data): array {
         $this->navbarPerson();
-        
         $this->content['main'][] = self::listAll($data, "person", _("List of persons"), [ "dateModified" => "Date modified" ]);
-        
         return $this->content;
     }
     
-    public function new(): array
-    {
+    public function new(): array {
         $this->navbarPerson();
-        
         $this->content['main'][] = [ "tag" => "h3", "content" => _("Add new person") ];
-        $this->content['main'][] = self::form();
-       
+        $this->content['main'][] = self::formPerson();
         return $this->content;
     }
     
-    public function edit(array $data): array 
-    {
+    public function edit(array $data): array {
         $value = $data[0];
-        
         $id = PropertyValue::extractValue($value['identifier'], "id");
-
         $this->navbarPerson($id, $value['name'] ?? "ND");
-        
-        // form
-        $this->content['main'][] = self::form('edit', $value);
-        
-        // contact point
+        // FORM
+        $this->content['main'][] = self::formPerson('edit', $value);
+        // CONTACT POINT
         $this->content['main'][] = self::divBoxExpanding(_("Contact point"), "ContactPoint", [ (new ContactPointView())->getForm('person', $id, $value['contactPoint']) ]);
-        
-        // address
+        // ADDRESS
         $this->content['main'][] = self::divBoxExpanding(_("Postal address"), "PostalAddress", [ (new PostalAddressView())->getForm("person", $id, $value['address']) ]);
-        
+        // IMAGE
+        $this->content['main'][] = self::divBoxExpanding(_("Image"), "ImageObject", [ (new ImageObjectView())->getForm("Person",$id,$value['image']) ]);
         return $this->content;
     }
     
-    private static function form($case = 'new', $value = null, $tableHasPart = null, $idHasPart = null)
-    {
+    private static function formPerson($case = 'new', $value = null, $tableHasPart = null, $idHasPart = null): array {
         $id = PropertyValue::extractValue($value['identifier'], 'id');
-        
-        $content[] = [ "tag" => "h6", "content" => ucfirst(_($case)) ]; 
-        
+        $content[] = [ "tag" => "h6", "content" => ucfirst(_($case)) ];
         $content[] = $case == "edit" ? [ "tag" => "input", "attributes" => [ "name"=>"id", "type" => "hidden", "value" => $id ] ] : null ;
-        
         if ($tableHasPart) {
             $content[] = [ "tag" => "input", "attributes" => [ "name"=>"tableHasPart", "type" => "hidden", "value"=>$tableHasPart ] ] ;
             $content[] = [ "tag" => "input", "attributes" => [ "name"=>"idHasPart", "type" => "hidden", "value"=>$idHasPart ] ] ;
         }
-        
-        // given name
+        // GIVEN NAME
         $attributes = [ "name"=>"givenName", "type" => "text", "value" => $value['givenName'] ?? "" ];
-        
         $attr = $case !== "edit" ? array_merge($attributes, [ "data-idselect" => "gv".($id ?? $case), "onKeyUp" => "selectItemFormBd(this,'person');", "autocomplete" => "off" ]) : $attributes;
-        
         $content[] = [ "tag" => "fieldset", "attributes" => [ "style" => "min-width: 380px;" ], "content" => [ 
             [ "tag" =>"legend", "content" => _("Given name") ],
             [ "tag" => "input", "attributes" => $attr ] 
         ]];
-        
         // family name
         $content[] = [ "tag" => "fieldset", "content" => [ 
             [ "tag" =>"legend", "content" => _("Family name") ],
             [ "tag" => "input", "attributes" => [ "name"=>"familyName", "type" => "text", "value"=>$value['familyName'] ] ] 
         ]];
-        
         // additional name
         $content[] = [ "tag" => "fieldset", "content" => [ 
             [ "tag" =>"legend", "content" => _("Additional name") ],
             [ "tag" => "input", "attributes" => [ "name"=>"additionalName", "type" => "text", "value"=>$value['additionalName'] ] ] 
         ]];
-        
         // Tax ID
         $content[] = [ "tag" => "fieldset", "content" => [ 
             [ "tag" =>"legend", "content" => _("Tax ID") ],
             [ "tag" => "input", "attributes" => [ "name"=>"taxId", "type" => "text", "value"=>$value['taxId'] ] ] 
         ]];
-        
         // birth date
         $content[] = [ "tag" => "fieldset", "content" => [ 
             [ "tag" =>"legend", "content" => _("Birth date") ],
