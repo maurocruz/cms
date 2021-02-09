08a05861df04dabd0bc6d121df7fb83399f3c2f0
Mauro Cruz <maurocruz@pirenopolis.tur.br>
2021-02-09 14:53:40 -0300

    Translate words with gettext

--- src/View/Html/Page/HistoryView.php
+++ src/View/Html/Page/HistoryView.php
@@ -6,10 +6,9 @@ use Plinct\Tool\DateTime;
 
 class HistoryView
 {    
-    public function view($data) 
+    public function view($data): array
     {
         $body = null;
-
         if (isset($data['message']) && $data['message'] == "No data founded") {
             $body[] = [ "tag" => "tr", "content" => [ 
                 [ "tag" => "td", "attributes" => [ "colspan" => "4" ], "content" => "Não há registros!" ] ] 
@@ -26,15 +25,14 @@ class HistoryView
         }
         
         return [ "tag" => "div", "attributes" => [ "class" => "box"], "content" => [
-            [ "tag" => "h4", "content" => "History" ],
+            [ "tag" => "h4", "content" => _("History") ],
             [ "tag" => "table", "attributes" => [ "class" => "contrato-table--history box " ], "content" => [
-
                 [ "tag" => "thead", "content" => [
                     [ "tag" => "tr", "content" => [
-                        [ "tag" => "th", "attributes" => [ "style" => "width: 160px;" ], "content" => "Data" ],
-                        [ "tag" => "th", "attributes" => [ "style" => "width: 80px;" ], "content" => "Acão" ],
-                        [ "tag" => "th", "content" => "Sumário" ],
-                        [ "tag" => "th", "attributes" => [ "style" => "width: 150px;" ], "content" => "autor" ]
+                        [ "tag" => "th", "attributes" => [ "style" => "width: 160px;" ], "content" => _("Date") ],
+                        [ "tag" => "th", "attributes" => [ "style" => "width: 80px;" ], "content" => _("Action") ],
+                        [ "tag" => "th", "content" => _("Summary") ],
+                        [ "tag" => "th", "attributes" => [ "style" => "width: 150px;" ], "content" => _("Author") ]
                     ]]
                 ]],
                 [ "tag" => "tbody", "content" => $body ]
