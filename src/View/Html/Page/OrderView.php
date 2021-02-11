837468f5816eb55153187c18054da54415df16d7
Mauro Cruz <maurocruz@pirenopolis.tur.br>
2021-02-11 13:59:31 -0300

    altered english words and columns of view

--- src/View/Html/Page/OrderView.php
+++ src/View/Html/Page/OrderView.php
@@ -24,7 +24,7 @@ class OrderView implements ViewInterface
             "/admin/order" => _("List all"),
             "/admin/order/new" => _("Add new"),
             "/admin/order/payment" => ucfirst(_("payments")),
-            "/admin/order/expired" => ucfirst(_("expired contracts"))
+            "/admin/order/expired" => ucfirst(_("Due dates"))
         ];
         $this->content['navbar'][] = navbarTrait::navbar($title, $list, $level);
     }
@@ -51,27 +51,21 @@ class OrderView implements ViewInterface
     public function new($data = null): array
     {
         $this->navbarOrder();
-
         $orderedItem = $data['orderedItem'] ?? null;
-
         if ($orderedItem) {
             $title = sprintf(_("New order for %s"), $orderedItem['@type']." \"".$orderedItem['name']."\"");
         } else {
             $title = _("New order");
         }
-
         $this->navbarOrder($title, [], 3);
-
         // order
         $this->content['main'][] = self::divBox(_("New order"), "order", [ self::formOrder("new", null, $orderedItem) ]);
-
         return $this->content;
     }
 
     public function edit(array $data): array
     {
         $this->navbarOrder();
-
         if (empty($data)) {
             $this->content['main'][] = self::noContent();
         } else {
@@ -79,27 +73,24 @@ class OrderView implements ViewInterface
             self::$idOrder = PropertyValue::extractValue($value['identifier'], "id");
             $title = sprintf(_("Order from '%s' for '%s'"), $value['customer']['name'], $value['seller']['name']);
             $this->navbarOrder($title, [], 3);
-
             // ORDER
             $this->content['main'][] = self::divBox(_("Order"), "order", [ self::formOrder("edit", $value) ]);
             // ORDERED ITEMS
             $this->content['main'][] = self::divBox(_("Ordered items"), "offer", [ OrderItemView::getForm($value) ]);
             // INVOICES
-            $this->content['main'][] = $value['orderedItem'] ? self::divBox(_("Invoices"), "invoice", [ InvoiceView::getForm("order", self::$idOrder, $value) ]) : null;
+            $this->content['main'][] = self::divBox(_("Invoices"), "invoice", [ InvoiceView::getForm("order", self::$idOrder, $value) ]);
             // HISTORY
             $this->content['main'][] = (new HistoryView())->view($value['history']);
             // banner
             $banner = $data['banner'] ?? null;
             $this->content['main'][] = $banner ? (new BannerView())->getBannerByIdcontrato($banner) : null;
         }
-
         return $this->content;
     }
 
     private function formOrder($case = "new", $value = null, $orderedItem = null): array
     {
         $content[] = $case == "edit" ? self::input("id", "hidden", self::$idOrder) : null;
-
         if ($orderedItem) {
             $orderedItemId = PropertyValue::extractValue($orderedItem['identifier'], "id");
             $providerId = PropertyValue::extractValue($orderedItem['provider']['identifier'], "id");
@@ -107,7 +98,6 @@ class OrderView implements ViewInterface
             $content[] = self::input("orderedItemType", "hidden", lcfirst($orderedItem['@type']));
             $content[] = self::input("seller", "hidden", $providerId);
         }
-
         // CUSTOMER
         $content[] = self::fieldset(self::chooseType("customer", "localBusiness,organization,person", $value['customer']), _("Customer"), [ "style" => "width: 100%;" ]);
         // SELLER
@@ -116,15 +106,14 @@ class OrderView implements ViewInterface
         $content[] = self::fieldsetWithInput(_("Order date"), "orderDate", $value['orderDate'] ? substr($value['orderDate'],0,10) : date("Y-m-d"), [], "date");
         // ORDER STATUS
         $content[] = self::fieldsetWithSelect(_("Order status"), "orderStatus", $value['orderStatus'], [
-            "OrderCancelled" => _("Order Cancelled"),
-            "OrderDelivered" => _("Order delivered"),
-            "OrderInTransit" => _("Order in transit"),
-            "OrderISuspended" => _("Order suspended"),
-            "OrderPaymentDue" => _("Order payment due"),
-            "OrderPickupAvailable" => _("Order pickup available"),
-            "OrderProblem" => _("Order problem"),
-            "OrderProcessing" => _("Order processing"),
-            "OrderReturned" => _("Order returned")
+            "OrderProcessing" => _("In processing"),
+            "OrderInTransit" => _("In transit"),
+            "OrderDelivered" => _("Delivered or performed"),
+            "OrderPickupAvailable" => _("Pickup available"),
+            "OrderSuspended" => _("Suspended"),
+            "OrderCancelled" => _("Cancelled"),
+            "OrderProblem" => _("With problem"),
+            "OrderReturned" => _("Returned")
         ]);
         // PAYMENT DUE DATE
         $content[] = self::fieldsetWithInput(_("Payment due date"), "paymentDueDate", substr($value['paymentDueDate'],0,10), [], "date");
@@ -136,12 +125,9 @@ class OrderView implements ViewInterface
         $content[] = self::fieldsetWithInput("Tipo ".$value['tipo']." (deprecated)", "tipo", $tipo, null, "text", [ "disabled" ]);
         // TAGS
         $content[] = self::fieldsetWithInput(_("Tags"), "tags", $value['tags'], [ "style" => "width: 100%;" ]);
-
         $submitAttributes = $case == "edit" ? [ "onclick" => "return setHistory(this.parentNode);" ] : null;
         $content[] = self::submitButtonSend($submitAttributes);
-
         $content[] = $case == "edit" ? self::submitButtonDelete("/admin/order/erase") : null;
-
         return self::form("/admin/order/$case", $content);
     }
 
@@ -149,46 +135,55 @@ class OrderView implements ViewInterface
     {
         $key = 0;
         $this->navbarOrder();
-
         $content[] = [ "tag" => "h3", "content" => _("Overdue payments") ];
         $content[] = self::selectPeriodo(count($data), "payment");
-
         $total = 0;
         foreach ($data as $key => $value) {
+            $idorder = $value['idorder'];
+            $order = "<a href=\"/admin/order/edit/$idorder\">"._("Edit")."</a>";
+            $orderStatus = _($value['orderStatus']);
+            $paymentDueDate = DateTime::formatDate($value['paymentDueDate']);
+            $totalPaymentDue = number_format($value['totalPaymentDue'],2,",",".");
+            $customerName = $value['customer']['name']." <a href=\"/admin/order/edit/".$value['idorder']."\">-></a>";
+            $invoiceInstallment = $value['numberOfTheInstallments']." / ".$value['totalOfInstallments'];
+            $orderedItem = $value['orderedItem'];
             $tbody[] = [ "tag" => "tr", "content" => [
-                [ "tag" => "td", "attributes" => [ "style" => "text-align: right"], "content" => DateTime::formatDate($value['paymentDueDate']) ],
-                [ "tag" => "td", "attributes" => [ "style" => "text-align: right"], "content" => number_format($value['totalPaymentDue'],2,",",".") ],
-                [ "tag" => "td", "content" => $value['name']." <a href=\"/admin/order/edit/".$value['idorder']."\">-></a>" ],
-                [ "tag" => "td", "content" => ($key+1)." / ".$value['number_parc'] ],
-                [ "tag" => "td", "content" => "<a href=\"/admin/order/edit/".$value['idorder']."\">".$value['contrato_name']."</a>" ],
-                [ "tag" => "td", "content" => $value['orderStatus'] == 'orderSuspended' ? 'Suspenso' : ($value['orderStatus'] == 'orderProcessing' ? 'Ativo' : 'Inativo') ]
+                [ "tag" => "td", "attributes" => [ "style" => "text-align: right"], "content" => $order],
+                [ "tag" => "td", "attributes" => [ "style" => "text-align: right"], "content" => $idorder],
+                [ "tag" => "td", "attributes" => [ "style" => "text-align: right"], "content" => $paymentDueDate],
+                [ "tag" => "td", "attributes" => [ "style" => "text-align: right"], "content" => $totalPaymentDue ],
+                [ "tag" => "td", "content" => $customerName ],
+                [ "tag" => "td", "content" => $invoiceInstallment ],
+                [ "tag" => "td", "content" => self::getOrderedItems($orderedItem) ],
+                [ "tag" => "td", "content" => $orderStatus ]
             ]];
             $total += $value['totalPaymentDue'];
         }
         // total
         $tbody[] = [ "tag" => "tr", "attributes" => [ "style" => "background-color: rgba(0,0,0,0.65);" ], "content" => [
+            [ "tag" => "td", "attributes" => [ "colspan" => "2"], "content" => "" ],
             [ "tag" => "td", "attributes" => [ "style" => "text-align: center"], "content" => "TOTAL" ],
-            [ "tag" => "td", "attributes" => [ "style" => "text-align: right"], "content" => "R$ ".number_format($total,2,",",".") ],
+            [ "tag" => "td", "attributes" => [ "style" => "text-align: right"], "content" => number_format($total,2,",",".") ],
             [ "tag" => "td", "attributes" => [ "style" => "text-align: center"], "content" => ($key+1). " itens" ],
             [ "tag" => "td", "content" => "" ],
             [ "tag" => "td", "content" => "" ],
             [ "tag" => "td", "content" => "" ]
         ]];
-
         $content[] = [ "tag" => "table", "attributes" => [ "class" => "table" ], "content" => [
             [ "tag" => "thead", "content" => [
                 [ "tag" => "tr", "content" => [
-                    [ "tag" => "th", "attributes" => [ "style" => "width: 115px;" ], "content" => _("Payment due date") ],
-                    [ "tag" => "th", "attributes" => [ "style" => "width: 100px;" ], "content" => _("Invoice amount") ],
-                    [ "tag" => "th", "content" => _("Customer") ],
-                    [ "tag" => "th", "attributes" => [ "style" => "width: 80px;" ], "content" => "Parcela" ],
-                    [ "tag" => "th", "attributes" => [ "style" => "width: 180px;" ], "content" => _("Contract type") ],
-                    [ "tag" => "th", "attributes" => [ "style" => "width: 180px;" ], "content" => _("Status") ]
+                    [ "tag" => "th", "attributes" => [ "style" => "width: 30px;" ], "content" => _("Action") ],
+                    [ "tag" => "th", "attributes" => [ "style" => "width: 30px;" ], "content" => _("ID") ],
+                    [ "tag" => "th", "attributes" => [ "style" => "width: 80px;" ], "content" => _("Due date") ],
+                    [ "tag" => "th", "attributes" => [ "style" => "width: 90px;" ], "content" => _("Values") ],
+                    [ "tag" => "th", "attributes" => [ "style" => "min-width: 240px;" ], "content" => _("Customer") ],
+                    [ "tag" => "th", "attributes" => [ "style" => "width: 80px;" ], "content" => ("Installments") ],
+                    [ "tag" => "th", "attributes" => [ "style" => "width: 240px;" ], "content" => _("Item") ],
+                    [ "tag" => "th", "attributes" => [ "style" => "width: 140px;" ], "content" => _("Status") ]
                 ]]
             ]],
             [ "tag" => "tbody", "content" => $tbody ]
         ] ];
-
         $content[] = [ "tag" => "p", "content" => "Imprimir", "href" => "javascript: void(0);", "hrefAttributes" => [ "onclick" => "print();" ] ];
         $this->content['main'][] = [ "tag" => "div", "attributes" => [ "class" => "box" ], "content" => $content ];
         return $this->content;
@@ -197,26 +192,32 @@ class OrderView implements ViewInterface
     public function expired($data): array
     {
         $tbody = null;
-
         $this->navbarOrder();
-
-        $content[] = [ "tag" => "h3", "content" => _("Expired orders") ];
-        $content[] = self::selectPeriodo(count($data), "expired");
-
-        foreach ($data as $key => $value) {
+        $content[] = [ "tag" => "h3", "content" => _("Expired or due orders") ];
+        $content[] = self::selectPeriodo($data['numberOfItems'], "expired");
+        foreach ($data['itemListElement'] as $key => $value) {
+            $item = $value['item'];
+            $id = $item['idorder'];
+            //var_dump($item);
             $tbody[] = [ "tag" => "tr", "content" => [
-                [ "tag" => "td", "attributes" => [ "style" => "text-align: right"], "content" => DateTime::formatDate($value['paymentDueDate']) ],
-                [ "tag" => "td", "content" => $value['name'] ],
-                [ "tag" => "td", "content" => "<a href=\"/admin/order/edit/".$value['idorder']."\">".$value['contrato_name']."</a>" ]
+                [ "tag" => "td", "attributes" => [ "style" => "text-align: right"], "content" => "<a href='/admin/order/edit/$id'>"._("Edit")."</a>" ],
+                [ "tag" => "td", "attributes" => [ "style" => "text-align: right"], "content" => $id ],
+                [ "tag" => "td", "attributes" => [ "style" => "text-align: right"], "content" => DateTime::formatDate($item['paymentDueDate']) ],
+                [ "tag" => "td", "content" => $item['customer']['name'] ],
+                [ "tag" => "td", "content" => self::getOrderedItems($item['orderedItem'])],
+                [ "tag" => "td", "content" => _($item['orderStatus'])]
             ]];
         }
 
         $content[] = [ "tag" => "table", "attributes" => [ "class" => "table" ], "content" => [
             [ "tag" => "thead", "content" => [
                 [ "tag" => "tr", "content" => [
-                    [ "tag" => "th", "attributes" => [ "style" => "width: 115px;" ], "content" => _("Payment due date") ],
+                    [ "tag" => "th", "attributes" => [ "style" => "width: 45px;" ], "content" => _("Action") ],
+                    [ "tag" => "th", "attributes" => [ "style" => "width: 50px;" ], "content" => _("ID") ],
+                    [ "tag" => "th", "attributes" => [ "style" => "width: 80px;" ], "content" => _("Due date") ],
                     [ "tag" => "th", "content" => _("Customer") ],
-                    [ "tag" => "th", "attributes" => [ "style" => "width: 180px;" ], "content" => _("Order item") ]
+                    [ "tag" => "th", "attributes" => [ "style" => "width: 240px;" ], "content" => _("Order item") ],
+                    [ "tag" => "th", "attributes" => [ "style" => "width: 145px;" ], "content" => _("Status") ]
                 ]]
             ]],
             [ "tag" => "tbody", "content" => $tbody ]
@@ -224,8 +225,18 @@ class OrderView implements ViewInterface
 
         $content[] = [ "tag" => "p", "content" => "Imprimir", "href" => "javascript: void(0);", "hrefAttributes" => [ "onclick" => "print();" ] ];
         $this->content['main'][] = [ "tag" => "div", "attributes" => [ "class" => "box" ], "content" => $content ];
-
         return $this->content;
+    }
+
+    private static function getOrderedItems($orderedItem): string
+    {
+        if (empty($orderedItem)) {
+            return _("Unidentified");
+        } else {
+            $quantityItems = count($orderedItem);
+            $firstItemName = $orderedItem[0]['orderedItem']['name'];
+            return $firstItemName . ($quantityItems >1 ? sprintf(_(" more % items."), $quantityItems-1) : null);
+        }
     }
 
     static private function selectPeriodo($numberOfItens, $section): array
