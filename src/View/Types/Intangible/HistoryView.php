548d6af1d7c63ab1913d90b7e670e8ca17be5aa2
Mauro Cruz <maurocruz@pirenopolis.tur.br>
2021-04-22 15:25:25 -0300

    upgrade write table

---
+++ src/View/Types/Intangible/HistoryView.php
@@ -0,0 +1,32 @@
+<?php
+namespace Plinct\Cms\View\Types\Intangible;
+
+use Plinct\Tool\DateTime;
+use Plinct\Web\Element\Table;
+
+class HistoryView {
+
+    public function view($data): array {
+        // TABLE
+        $table = new Table();
+        // HEADERS
+        $table->head(_("Date"), [ "style" => "width: 160px;" ])
+            ->head(_("Action"), [ "style" => "width: 80px;" ])
+            ->head(_("Summary"))
+            ->head(_("Author"), [ "style" => "width: 150px;" ]);
+        // BODY
+        if($data) {
+            foreach ($data as $value) {
+                $table->bodyCell(DateTime::formatDateTime($value['datetime']))
+                    ->bodyCell($value['action'])
+                    ->bodyCell(stripslashes($value['summary']))
+                    ->bodyCell($value['user'])
+                    ->closeRow();
+            }
+        } else {
+            $table->bodyCell(_("No data found!"), [ "colspan" => "4", "style" => "text-align: center;" ])->closeRow();
+        }
+        // READY
+        return $table->ready();
+    }
+}
