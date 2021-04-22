092068a6dedccb3478853c773b5e5b9ca156ab7d
Mauro Cruz <maurocruz@pirenopolis.tur.br>
2021-04-22 15:36:14 -0300

    upgrade to new view

--- src/View/Types/Intangible/Service/ServiceView.php
+++ src/View/Types/Intangible/Service/ServiceView.php
@@ -5,7 +5,6 @@ use Plinct\Cms\View\Html\Page\ViewInterface;
 use Plinct\Cms\View\Types\Intangible\Offer\OfferView;
 use Plinct\Cms\View\Widget\HtmlPiecesTrait;
 use Plinct\Cms\View\Widget\navbarTrait;
-use Plinct\Tool\ArrayTool;
 
 class ServiceView extends ServiceWidget implements ViewInterface {
 
@@ -17,43 +16,43 @@ class ServiceView extends ServiceWidget implements ViewInterface {
     }
 
     public function index(array $data): array {
-        $columnRows = [
-            "name" => _("Name"),
-            "additionalType" => _("AddtionalType"),
-            "category" => _("Category"),
-            "dateCreated" => _("Date created"),
-            "dateModified" => _("Date modified")
-        ];
-        $this->content['main'][] = HtmlPiecesTrait::indexWithSubclass($data['name'], "services", $columnRows, $data['services']['itemListElement'] );
+        $this->content['main'][] = parent::noContent("Method not done!");
         return $this->content;
     }
 
     public function new($data = null): array {
+        return $this->content;
+    }
+    public function edit(array $data): array {
+        $this->content['main'][] = parent::noContent("Method not done!");
+        return $this->content;
+    }
+
+    public function indexWithPropertyOf(array $data): array {
+        $columnRows = [
+            "name" => _("Name"),
+            "category" => _("Category"),
+            "serviceType" => [ _("Service type"), [ "style" => "width: 200px;"] ],
+            "dateModified" => [ _("Date modified"), [ "style" => "width: 150px;"] ]
+        ];
+        $this->content['main'][] = HtmlPiecesTrait::indexWithSubclass($data['name'], "service", $columnRows, $data['services']['itemListElement'] );
+        return $this->content;
+    }
+
+    public function newWithPropertyOf($data = null): array {
         $this->content['main'][] = self::divBox2(sprintf(_("Add new %s from %s"), _("service"), $data['provider']['name']), [ parent::serviceForm("new", $data) ]);
         return $this->content;
     }
 
-    public function edit(array $data): array {
+    public function editWithPropertyOf(array $data): array {
         if (empty($data)) {
             $this->content['main'][] = self::noContent();
         } else {
             // EDIT SERVICE
             $this->content['main'][] = self::divBox(_("Edit"), "service", [ self::serviceForm("edit", $data) ]);
             // OFFER
-            $this->content['main'][] = self::divBox(_("Offer"), "offer", [ (new OfferView())->edit($data) ]);
+            $this->content['main'][] = self::divBoxExpanding(_("Offer"), "offer", [ (new OfferView())->edit($data) ]);
         }
-        return $this->content;
-    }
-
-    public function order($value): array {
-        $this->id = ArrayTool::searchByValue($value['identifier'], "id")['value'];
-        $this->navbarService($value['name']);
-        $this->content['main'][] = self::listAll($value['orders'], "order", sprintf(_("'%s' service order list"), $value['name']), [
-            "orderDate" => _("Order date"),
-            "customer" => _("Customer"),
-            "seller" => _("Seller"),
-            "orderStatus" => _("Order status"),
-        ]);
         return $this->content;
     }
 
