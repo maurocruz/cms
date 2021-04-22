31049de20d120711cf633508e4b437a25aea8efb
Mauro Cruz <maurocruz@pirenopolis.tur.br>
2021-04-22 15:33:40 -0300

    upgrade for new view

--- src/View/Types/Organization/OrganizationWidget.php
+++ src/View/Types/Organization/OrganizationWidget.php
@@ -1,15 +1,19 @@
 <?php
-
 namespace Plinct\Cms\View\Types\Organization;
 
+use Plinct\Cms\View\Types\Intangible\Order\OrderView;
+use Plinct\Cms\View\Types\Intangible\Service\ServiceView;
+use Plinct\Cms\View\Types\Product\ProductView;
 use Plinct\Cms\View\Widget\FormElementsTrait;
 use Plinct\Cms\View\Widget\navbarTrait;
 use Plinct\Tool\ArrayTool;
 
 abstract class OrganizationWidget {
-    protected $content;
+    protected $content = [];
     protected $id;
     protected $name;
+    protected $idItem;
+    protected $nameItem;
 
     use navbarTrait;
     use FormElementsTrait;
@@ -46,23 +50,18 @@ abstract class OrganizationWidget {
         $this->navbarIndex();
         $subMenus = [
             "/admin/organization/edit?id=$this->id" => _("View it"),
-            "/admin/organization/service?id=$this->id" => _("Service"),
-            "/admin/organization/product?id=$this->id" => _("Product"),
-            "/admin/organization/offer?id=$this->id" => _("Offer")
+            "/admin/organization/service?id=$this->id" => _("Services"),
+            "/admin/organization/product?id=$this->id" => _("Products"),
+            "/admin/organization/order?id=$this->id" => _("Orders")
         ];
         $this->addContent('navbar', self::navbar($this->name, $subMenus, 3));
     }
 
-    protected function navbarService() {
+    protected function navbarItem($itemType) {
         $this->navbarEdit();
-        $this->addContent('navbar', self::navbar(_("Services"), [ "?id=$this->id" => _("List all"), "?id=$this->id&action=new" => sprintf(_("Add new %s"), _("service")) ], 4));
-    }
-
-    protected function navbarProduct($name = null) {
-        $this->navbarEdit();
-        $this->addContent('navbar', self::navbar(_("Products"), [ "?id=$this->id" => _("List all"), "?id=$this->id&action=new" => sprintf(_("Add new %s"), _("product")) ], 4));
-        if ($name) {
-            $this->addContent('navbar', self::navbar($name, [], 5));
+        $this->addContent('navbar', self::navbar(_($itemType), [ "?id=$this->id" => _("List all"), "?id=$this->id&action=new" => sprintf(_("Add new %s"), _(lcfirst($itemType))) ], 4));
+        if ($this->nameItem) {
+            $this->addContent('navbar', self::navbar($this->nameItem, [], 5));
         }
     }
 
@@ -96,5 +95,51 @@ abstract class OrganizationWidget {
             $content[] = self::submitButtonDelete('/admin/organization/delete');
         }
         return [ "tag" => "form", "attributes" => [ "name" => "form-organization", "id" => "form-organization", "class" => "formPadrao", "action" => "/admin/organization/$case", "method" => "post" ], "content" => $content ];
+    }
+
+    protected function itemView(string $itemType, $data): array {
+        $itemProperty = null;
+        $itemView = null;
+        $value = $data[0];
+        $action = filter_input(INPUT_GET, 'action');
+        $itemText = lcfirst($itemType)."s";
+        // SET ITEM PROPERTY
+        if ($itemType == "Product") {
+            $itemProperty = "manufacturer";
+            $itemView = new ProductView();
+        }
+        if ($itemType == "Service") {
+            $itemProperty = "provider";
+            $itemView = new ServiceView();
+        }
+        if ($itemType == "Order") {
+            $itemProperty = "seller";
+            $itemView = new OrderView();
+        }
+        // NO CONTENT
+        if (!$itemProperty || !is_object($itemView)) {
+            $this->addContent('main', self::noContent("No item detected"));
+            return $this->content;
+        }
+        // CONTINUE SET PROPERTIES
+        $owner = $action !== "new" ? ($value[$itemProperty] ?? null) : null;
+        $this->id = $owner ? ArrayTool::searchByValue($owner['identifier'], "id")['value'] : ArrayTool::searchByValue($value['identifier'], "id")['value'];
+        $this->name = $owner ? $owner['name'] : $value['name'];
+        $this->idItem = $owner ? ArrayTool::searchByValue($value['identifier'], "id")['value'] : null;
+        $this->nameItem = $owner ? ($value['name'] ?? null) : null;
+        // NAVBAR
+        $this->navbarItem($itemType);
+        // SWITCH
+        if ($action == "new") {
+            $this->addContent('main', $itemView->newWithPropertyOf([ $itemProperty => $value ]));
+        } elseif ($value['@type'] == "Organization" && empty($value[$itemText])) {
+            $this->addContent('main', [ "tag" => "p", "content" => _("No $itemText added") ]);
+        } elseif ($value['@type'] == $itemType) {
+            $this->addContent('main', $itemView->editWithPropertyOf($value));
+        } else {
+            $this->addContent('main', $itemView->indexWithPropertyOf($value));
+        }
+        // RESPONSE
+        return $this->content;
     }
 }
