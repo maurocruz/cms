cd82269f6a07208cf65d7cee06cc3bf452584422
Mauro Cruz <maurocruz@pirenopolis.tur.br>
2021-04-22 15:24:29 -0300

    Add new class server

---
+++ src/Server/HistoryServer.php
@@ -0,0 +1,45 @@
+<?php
+namespace Plinct\Cms\Server;
+
+use Plinct\Cms\App;
+
+class HistoryServer {
+    private $tableHasPart;
+    private $idHasPart;
+    private $action;
+    private $summary;
+    private $userId;
+
+    public function __construct($tableHasPart, $idHasPart) {
+        $this->tableHasPart = $tableHasPart;
+        $this->idHasPart = $idHasPart;
+        $this->userId = App::getUserId();
+    }
+
+    public function setSumaryByDifference(array $params, array $data) {
+        $text = '';
+        $data = array_filter($data, function ($var) { return(!is_array($var));  });
+        $diff = array_diff($params, $data);
+        foreach ($diff as $key => $value) {
+            $text .= $key.": {$data[$key]} to $value; ";
+        }
+        $this->summary = $text;
+    }
+
+    public function register($action, $summary = null) {
+        $this->action = $action;
+        $this->summary = $summary ?? $this->summary;
+        $params = $this->getParams();
+        var_dump($params);
+        return Api::post("history", $params);
+    }
+
+    private function getParams(): array {
+        $params['tableHasPart'] = $this->tableHasPart;
+        $params['idHasPart'] = $this->idHasPart;
+        $params['action'] = $this->action;
+        $params['summary'] = $this->summary;
+        $params['user'] = $this->userId;
+        return $params;
+    }
+}
