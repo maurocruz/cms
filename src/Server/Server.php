ff725caab9c69a5af53fcb2a9071e5fcd347af09
Mauro Cruz <maurocruz@pirenopolis.tur.br>
2021-04-22 15:35:43 -0300

    upgrade to new view

--- src/Server/Server.php
+++ src/Server/Server.php
@@ -1,29 +1,60 @@
 <?php
 namespace Plinct\Cms\Server;
 
-class Server extends Api {
+class Server {
     private $tableHasPart;
 
     public function edit($type, $params) {
-        parent::put($type, $params);
+        $classType = __NAMESPACE__."\\".ucfirst($type)."Server";
+        if (class_exists($classType)) {
+            return (new $classType())->edit($params);
+        }
+        Api::put($type, $params);
         return filter_input(INPUT_SERVER, 'HTTP_REFERER');
     }
 
     public function new($type, $params): string {
-        // API
-        $data = parent::post($type, $params);
-        // REDIRECT TO EDIT PAGE
-        if (isset($data['id']) && !isset($params['tableHasPart'])) {
-            return dirname(filter_input(INPUT_SERVER, 'REQUEST_URI')) . DIRECTORY_SEPARATOR . "edit" . DIRECTORY_SEPARATOR . $data['id'];
+        // ORDER ITEM
+        if ($type == "orderItem") return OrderItemServer::new($params);
+        // UPLOAD IMAGE
+        if ($type == "imageObject" && isset($_FILES['imageupload'])) {
+            if ($_FILES['imageupload']['size'][0] === 0) {
+                return self::httpReferrer();
+            }
+            $newParams = ImageObjectServer::uploadImages($_FILES['imageupload'], $params['location']);
+            unset($params['location']);
+            foreach ($newParams as $valueNewParams) {
+                $params = array_merge($params, $valueNewParams);
+                if (!Api::post($type, $params)) {
+                    die("error!");
+                }
+            }
+            return self::httpReferrer();
+        } else {
+            // API
+            $data = Api::post($type, $params);
+            if ($type == "product") {
+                return self::httpReferrer();
+            }
+            // REDIRECT TO EDIT PAGE
+            if (isset($data['id']) && !isset($params['tableHasPart'])) {
+                return dirname(filter_input(INPUT_SERVER, 'REQUEST_URI')) . DIRECTORY_SEPARATOR . "edit" . DIRECTORY_SEPARATOR . $data['id'];
+            }
+            $this->unsetRelParams($params);
+            return $this->return();
         }
-        $this->unsetRelParams($params);
-        return $this->return();
     }
     
     public function erase($type, $params): string {
-        parent::delete($type, $params);
-        $this->unsetRelParams($params);
-        return $this->return();
+        // API ACTION
+        $response = Api::delete($type, [ "id" => $params['id'] ]);
+        // RESPONSE REDIRECT
+        if (isset($response['message']) && $response['message'] == "Deleted successfully") {
+            return isset($params['tableHasPart']) ? self::httpReferrer() : self::requestUri();
+        } else {
+            var_dump([ "error" => [ "response" => $response ]]);
+            die;
+        }
     }
 
     public function createSqlTable($type) {
@@ -31,20 +62,27 @@ class Server extends Api {
         (new $classname())->createSqlTable($type);
     }
 
-    private function unsetRelParams($params) {
+    private function unsetRelParams($params): void {
         $this->tableHasPart = $params['tableHasPart'] ?? null;
         unset($params['tableHasPart']);
         unset($params['idHasPart']);
         unset($params['tableIsPartOf']);
         unset($params['idIsPartOf']);
-        return $params;
     }
-    
+
+    private static function httpReferrer(): string {
+        return filter_input(INPUT_SERVER, 'HTTP_REFERER');
+    }
+
+    private static function requestUri(): string {
+        return dirname(filter_input(INPUT_SERVER, 'REQUEST_URI'));
+    }
+
     private function return(): string {
         if ($this->tableHasPart) {
-            return filter_input(INPUT_SERVER, 'HTTP_REFERER');
+            return self::httpReferrer();
         } else {                
-            return dirname(filter_input(INPUT_SERVER, 'REQUEST_URI'));
+            return self::requestUri();
         }
     }
 }
