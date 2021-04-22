46a553524c46bdd41c3e5c4ac8b3fd702ad0583e
Mauro Cruz <maurocruz@pirenopolis.tur.br>
2021-04-22 15:27:45 -0300

    upgrade for view only propertyOf

---
+++ src/View/Types/Intangible/Invoice/InvoiceView.php
@@ -0,0 +1,27 @@
+<?php
+namespace Plinct\Cms\View\Types\Intangible\Invoice;
+
+class InvoiceView extends InvoiceWidget {
+
+    public function edit(array $data): array {
+        $this->idorder = $data['idorder'];
+        $lenght = $data['partOfInvoice'] ? count($data['partOfInvoice']): 0;
+        // NEW
+        $content[] = parent::formInvoice("new", null, $lenght+1 );
+        // INVOICES
+        if ($lenght > 0) {
+            foreach ($data['partOfInvoice'] as $key => $value) {
+                // SET TOTALS AMOUNT
+                $this->totalInvoiceAmount += $value['totalPaymentDue'];
+                $this->totalPaidAmount += $value['paymentDate'] !== '0000-00-00' ? $value['totalPaymentDue'] : 0;
+                $this->totalPayableAmount += $value['paymentDate'] == '0000-00-00' ? $value['totalPaymentDue'] : 0;
+                $this->totalPastDueAmount += $value['paymentDate'] == '0000-00-00' && date("Y-m-d") > $value['paymentDueDate'] ? $value['totalPaymentDue'] : 0;
+                // FORM
+                $content[] = parent::formInvoice('edit', $value, $lenght - $key);
+            }
+        }
+        // balance
+        $content[] = parent::balance();
+        return $content;
+    }
+}
