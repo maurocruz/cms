014abfe6e4043ec71530483f43243fdd97b9791d
Mauro Cruz <maurocruz@pirenopolis.tur.br>
2021-04-22 15:25:57 -0300

    add new class server

---
+++ src/Server/InvoiceServer.php
@@ -0,0 +1,18 @@
+<?php
+namespace Plinct\Cms\Server;
+
+class InvoiceServer {
+
+    public function edit($params) {
+        // REGISTER HISTORY IN ORDER REFERENCE
+        $history = new HistoryServer('order', $params['referencesOrder']);
+        // GET OLDER DATA
+        $data = Api::get('invoice', [ "id" => $params['id'] ]);
+        // COMPARE NEW DATA
+        $history->setSumaryByDifference($params, $data[0]);
+        // REGISTER HISTORY
+        $history->register("UPDATE");
+        Api::put("invoice", $params);
+        return filter_input(INPUT_SERVER, 'HTTP_REFERER');
+    }
+}
