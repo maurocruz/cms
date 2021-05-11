c294e3be5fb35471b50a263a329b37c2b519d116
Mauro Cruz <maurocruz@pirenopolis.tur.br>
2021-05-11 17:14:23 -0300

    Authentication.php - insert condition if api secret key not set

--- src/Middleware/Authentication.php
+++ src/Middleware/Authentication.php
@@ -15,18 +15,20 @@ class Authentication implements MiddlewareInterface {
      * @return ResponseInterface
      */
     public function process(ServerRequestInterface $request, RequestHandlerInterface $handler): ResponseInterface {
-        //var_dump(session_status());
-        //var_dump(PHP_SESSION_NONE);
         if (session_status() === PHP_SESSION_NONE) session_start();
         $token = $_COOKIE['API_TOKEN'] ?? null;
-        $tokenDecode = $token ? JWT::decode($token, App::getApiSecretKey(), ["HS256"]) : null;
-        $userLogin = $_SESSION['userLogin'] ?? null;
-        if ($token && !$userLogin) {
-            $_SESSION['userLogin']['name'] = $tokenDecode->name;
-            $_SESSION['userLogin']['admin'] = $tokenDecode->admin;
-            $_SESSION['userLogin']['uid'] = $tokenDecode->uid;
-        } elseif(!$token && isset($userLogin) ) {
-            unset($_SESSION['userLogin']);
+        if (App::getApiSecretKey()) {
+            $tokenDecode = $token ? JWT::decode($token, App::getApiSecretKey(), ["HS256"]) : null;
+            $userLogin = $_SESSION['userLogin'] ?? null;
+            if ($token && !$userLogin) {
+                $_SESSION['userLogin']['name'] = $tokenDecode->name;
+                $_SESSION['userLogin']['admin'] = $tokenDecode->admin;
+                $_SESSION['userLogin']['uid'] = $tokenDecode->uid;
+            } elseif (!$token && isset($userLogin)) {
+                unset($_SESSION['userLogin']);
+            }
+        } else {
+            $request = $request->withAttribute("ApiSecretKey", false);
         }
         return $handler->handle($request);
     }
