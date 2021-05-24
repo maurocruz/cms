cdbd27f96480de605ea23ec06ca2918d086ed60e
Mauro Cruz <maurocruz@pirenopolis.tur.br>
2021-05-24 19:24:48 -0300

    Server.php - Ordered functions

--- src/Server/Server.php
+++ src/Server/Server.php
@@ -4,18 +4,6 @@ namespace Plinct\Cms\Server;
 class Server extends Api {
     private $tableHasPart;
 
-    public function edit($type, $params) {
-        $className = __NAMESPACE__."\\Type\\".ucfirst($type)."Server";
-        if (class_exists($className)) {
-            $object = new $className();
-            if (method_exists($object,"edit")) {
-                return $object->edit($params);
-            }
-        }
-        Api::put($type, $params);
-        return filter_input(INPUT_SERVER, 'HTTP_REFERER');
-    }
-
     public function new($type, $params): string {
         $classTypeServer = __NAMESPACE__."\\Type\\".ucfirst($type)."Server";
         if (class_exists($classTypeServer)) {
@@ -29,6 +17,18 @@ class Server extends Api {
         }
         $this->unsetRelParams($params);
         return $this->return();
+    }
+
+    public function edit($type, $params) {
+        $className = __NAMESPACE__."\\Type\\".ucfirst($type)."Server";
+        if (class_exists($className)) {
+            $object = new $className();
+            if (method_exists($object,"edit")) {
+                return $object->edit($params);
+            }
+        }
+        Api::put($type, $params);
+        return filter_input(INPUT_SERVER, 'HTTP_REFERER');
     }
     
     public function erase($type, $params): string {
