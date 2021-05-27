9f88b5e7e3d5faf2289a4ba5ddf7e02f8c27116f
Mauro Cruz <maurocruz@pirenopolis.tur.br>
2021-05-27 16:06:46 -0300

    OrderServer.php - added and update methods

--- src/Server/Type/OrderServer.php
+++ src/Server/Type/OrderServer.php
@@ -1,10 +1,10 @@
 <?php
 namespace Plinct\Cms\Server\Type;
 
-use Plinct\Cms\App;
 use Plinct\Cms\Server\Api;
+use Plinct\Cms\Server\ServerAbstract;
 
-class OrderServer {
+class OrderServer extends ServerAbstract {
 
     public function new($params): string {
         $seller = $params['seller'];
@@ -12,16 +12,28 @@ class OrderServer {
         // insert new order
         $data = Api::post('order', $params);
         $id = $data['id'];
-        // insert new history
-        $historyParams = [
-            "tableHasPart" => "order",
-            "idHasPart" => $id,
-            "action" => "CREATED",
-            "summary" => _("Created new order"),
-            "datetime" => date("Y-m-d H:i:s"),
-            "user" => App::getUserLoginId()
-        ];
-        Api::post('history', $historyParams);
-        return "/admin/" . lcfirst($sellerType) . "/order?id=$seller&item=$id";
+        // REGISTER HISTORY IN ORDER REFERENCE
+        $history = new HistoryServer('order', $id);
+        $history->setSummary("Created new order");
+        $history->register("CREATED");
+        // RESPONSE
+        $redirect = "/admin/" . lcfirst($sellerType) . "/order?id=$seller&item=$id";
+        return parent::response($data, $redirect);
+    }
+
+    public function edit($params) {
+        // HISTORY
+        $dataOld = Api::get('order', [ "id" => $params['id'] ]);
+        parent::setHistoryUpdateWithDifference('order', $params['id'], $dataOld[0], $params);
+        // RESPONSE
+        return parent::response(Api::put('order',$params));
+    }
+
+    public function erase($params) {
+        $idorder = $params['id'];
+        $tableHasPart = $params['tableHasPart'] ?? lcfirst($params['sellerType']);
+        $idHaspart = $params['seller'];
+        $redirect = "/admin/$tableHasPart/order?id=$idHaspart";
+        return parent::response(Api::delete('order', [ "id" => $idorder ]), $redirect);
     }
 }
