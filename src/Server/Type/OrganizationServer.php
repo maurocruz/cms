a766c8e515645ccbc812f55d522a321825584a79
Mauro Cruz <maurocruz@pirenopolis.tur.br>
2021-05-27 16:07:46 -0300

    OrganizationServer.php - added condition if isset additional type;

--- src/Server/Type/OrganizationServer.php
+++ src/Server/Type/OrganizationServer.php
@@ -6,7 +6,9 @@ use Plinct\Cms\Server\Api;
 class OrganizationServer {
 
     public function edit($params) {
-        $params['additionalType'] = str_replace(" -> ",",",$params['additionalType']);
+        if (isset($params['additionalType'])) {
+            $params['additionalType'] = str_replace(" -> ",",",$params['additionalType']);
+        }
         Api::put('organization', $params);
         return filter_input(INPUT_SERVER, 'HTTP_REFERER');
     }
