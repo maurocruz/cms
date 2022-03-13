f210c8e9aa01a8a4d84c2c40950ce246e6d86940
Mauro Cruz <maurocruz@pirenopolis.tur.br>
2022-03-13 16:05:50 -0300

    Added subEvent properties

--- src/WebSite/Type/Event/EventView.php
+++ src/WebSite/Type/Event/EventView.php
@@ -8,7 +8,6 @@ use Exception;
 use Plinct\Cms\WebSite\Fragment\Fragment;
 use Plinct\Cms\WebSite\Type\ImageObject\ImageObjectView;
 use Plinct\Cms\WebSite\Type\View;
-use Plinct\Tool\ArrayTool;
 
 class EventView extends EventAbstract
 {
@@ -46,37 +45,34 @@ class EventView extends EventAbstract
     {
         // NAVBAR
         $this->navbarEvent();
-
         // FORM
         View::main(Fragment::box()->simpleBox(parent::formEvent(), _("Add new")));
     }
 
-    /**
-     * @throws Exception
-     */
-    public function edit(array $data)
-    {
-        // NAVBAR
-        $this->navbarEvent();
+  /**
+   * @throws Exception
+   */
+  public function edit(array $data)
+  {
+    // NAVBAR
+    $this->navbarEvent();
 
-        if (!$data) {
-            View::main(Fragment::miscellaneous()->message(_("Event not found")));
+    if (!$data) {
+      View::main(Fragment::miscellaneous()->message(_("Event not found")));
 
-        } else {
-            $value = $data[0];
-            $this->idEvent = (int)ArrayTool::searchByValue($value['identifier'], 'id')['value'];
-
-            // VIEW IN SITE
-            View::main([ "tag" => "p", "content" => _("View on website"), "href" => "/eventos/". substr($value['startDate'], 0, 10)."/". urlencode($value['name']), "hrefAttributes" => [ "target" => "_blank" ] ]);
-
-            // EVENT FORM
-            View::main(Fragment::box()->simpleBox(self::formEvent('edit', $value), _("Edit event")));
-
-            // PLACE
-            View::main(Fragment::box()->expandingBox(_("Place"), Fragment::form()->relationshipOneToOne("event", (string) $this->idEvent, "location", "place", $value['location'])));
-
-            // IMAGE
-            View::main(Fragment::box()->expandingBox(_("Images"), (new ImageObjectView())->getForm("event", $this->idEvent, $value['image'])));
-        }
+    } else {
+      $value = $data[0];
+      $this->idevent = $value['idevent'];
+      // VIEW IN SITE
+      View::main([ "tag" => "p", "content" => _("View on website"), "href" => "/eventos/". substr($value['startDate'], 0, 10)."/". urlencode($value['name']), "hrefAttributes" => [ "target" => "_blank" ] ]);
+      // EVENT FORM
+      View::main(Fragment::box()->simpleBox(self::formEvent('edit', $value), _("Edit event")));
+			// SUB EVENTS
+      View::main(Fragment::box()->expandingBox(_("Sub Events"), Fragment::form()->relationshipOneToMany("event", $this->idevent, 'event', $value['subEvent'], "idevent desc")));
+      // PLACE
+      View::main(Fragment::box()->expandingBox(_("Place"), Fragment::form()->relationshipOneToOne("event", $this->idevent, "location", "place", $value['location'])));
+      // IMAGE
+      View::main(Fragment::box()->expandingBox(_("Images"), (new ImageObjectView())->getForm("event", (int) $this->idevent, $value['image'])));
     }
+  }
 }
