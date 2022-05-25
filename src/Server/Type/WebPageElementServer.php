e9a468e3c24cbe5254a9d991d492d9768bd48509
Mauro Cruz <maurocruz@pirenopolis.tur.br>
2022-05-25 13:53:21 -0300

    Change id property name

--- src/Server/Type/WebPageElementServer.php
+++ src/Server/Type/WebPageElementServer.php
@@ -8,45 +8,38 @@ use Plinct\Cms\Server\Api;
 
 class WebPageElementServer
 {
-    /**
-     * @param $params
-     * @return mixed
-     */
-    public function new($params)
-    {
-        Api::post("webPageElement", $params);
+  /**
+   * @param $params
+   * @return mixed
+   */
+  public function new($params)
+  {
+    Api::post("webPageElement", $params);
+    Api::put("webPage", ['idwebPageElement'=>$params['idHasPart'], 'dateModified'=>date('Y-m-d H:i:s') ]);
+    return filter_input(INPUT_SERVER, 'HTTP_REFERER');
+  }
 
-        Api::put("webPage", [ 'id'=>$params['idHasPart'], 'dateModified'=>date('Y-m-d H:i:s') ]);
+  /**
+   * @param $params
+   * @return mixed
+   */
+  public function edit($params)
+  {
+    $idHasPart = $params['idHasPart'];
+    unset($params['idHasPart']);
+    Api::put("webPageElement", $params);
+    Api::put("webPage", ['idwebPageElement'=>$idHasPart, 'dateModified'=>date('Y-m-d H:i:s') ]);
+    return filter_input(INPUT_SERVER, 'HTTP_REFERER');
+  }
 
-        return filter_input(INPUT_SERVER, 'HTTP_REFERER');
-    }
-
-    /**
-     * @param $params
-     * @return mixed
-     */
-    public function edit($params)
-    {
-        $idHasPart = $params['idHasPart'];
-        unset($params['idHasPart']);
-
-        Api::put("webPageElement", $params);
-
-        Api::put("webPage", [ 'id'=>$idHasPart, 'dateModified'=>date('Y-m-d H:i:s') ]);
-
-        return filter_input(INPUT_SERVER, 'HTTP_REFERER');
-    }
-
-    /**
-     * @param $params
-     * @return mixed
-     */
-    public function erase($params)
-    {
-        Api::delete('webPageElement', ['id'=>$params['id']]);
-
-        Api::put("webPage", [ 'id'=>$params['idHasPart'], 'dateModified'=>date('Y-m-d H:i:s') ]);
-
-        return filter_input(INPUT_SERVER, 'HTTP_REFERER');
-    }
+  /**
+   * @param $params
+   * @return mixed
+   */
+  public function erase($params)
+  {
+    Api::delete('webPageElement', ['idwebPageElement'=>$params['idwebPageElement'] ?? $params['id']]);
+    Api::put("webPage", ['idwebPageElement'=>$params['idHasPart'], 'dateModified'=>date('Y-m-d H:i:s') ]);
+    return filter_input(INPUT_SERVER, 'HTTP_REFERER');
+  }
 }
