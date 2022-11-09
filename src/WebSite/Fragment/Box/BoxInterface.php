aa4458dbcec7f798fe71a61236e8b92a82f5b716
Mauro Cruz <maurocruz@pirenopolis.tur.br>
2022-11-09 09:56:23 -0300

    Added attributes in simple box

--- src/WebSite/Fragment/Box/BoxInterface.php
+++ src/WebSite/Fragment/Box/BoxInterface.php
@@ -6,17 +6,18 @@ namespace Plinct\Cms\WebSite\Fragment\Box;
 
 interface BoxInterface
 {
-    /**
-     * @param $content
-     * @param string|null $caption
-     * @return array
-     */
-    public function simpleBox($content, string $caption = null): array;
+	/**
+	 * @param $content
+	 * @param string|null $caption
+	 * @param array $attributes
+	 * @return array
+	 */
+    public function simpleBox($content, string $caption = null, array $attributes = ['class'=>'box']): array;
 
-    /**
-     * @param string $caption
-     * @param $content
-     * @return array
-     */
-    public function expandingBox(string $caption, $content): array;
+  /**
+   * @param string $caption
+   * @param $content
+   * @return array
+   */
+  public function expandingBox(string $caption, $content): array;
 }
