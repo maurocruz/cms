f10dfed9dc4fe5876205bfff824417bfd1db8a3a
Mauro Cruz <maurocruz@pirenopolis.tur.br>
2022-11-09 09:54:55 -0300

    Added functions for edit anda delete user

--- src/Server/Type/UserServer.php
+++ src/Server/Type/UserServer.php
@@ -8,15 +8,52 @@ use Plinct\Cms\Server\Api;
 
 class UserServer
 {
-    public function new(array $params)
-    {
-        // API
-        $data = Api::post('user', $params);
+	/**
+	 * @param array $params
+	 * @return string|void
+	 */
+  public function new(array $params)
+  {
+    // API
+    $data = Api::post('user', $params);
 
-        if(isset($data['id'])) {
-            return dirname(filter_input(INPUT_SERVER, 'REQUEST_URI')) . DIRECTORY_SEPARATOR . "edit" . DIRECTORY_SEPARATOR . $data['id'];
-        } elseif(isset($data['status'])) {
-            return filter_input(INPUT_SERVER, 'HTTP_REFERER') . "?" . http_build_query($data);
-        }
+    if(isset($data['id'])) {
+        return dirname(filter_input(INPUT_SERVER, 'REQUEST_URI')) . DIRECTORY_SEPARATOR . "edit" . DIRECTORY_SEPARATOR . $data['id'];
+    } elseif(isset($data['status'])) {
+        return filter_input(INPUT_SERVER, 'HTTP_REFERER') . "?" . http_build_query($data);
     }
+		print_r($data);
+		die();
+  }
+
+	/**
+	 * @param array $params
+	 * @return mixed|void
+	 */
+	public function edit(array $params)
+	{
+		$data = Api::put('user',$params);
+		if ($data['status'] == "success") {
+			return filter_input(INPUT_SERVER,'HTTP_REFERER');
+		}
+		print_r($data);
+		die();
+	}
+
+	/**
+	 * @param array $params
+	 * @return string|void
+	 */
+	public function erase(array $params)
+	{
+		if (isset($params['iduser'])) {
+			$data = Api::delete('user', ['iduser'=>$params['iduser']]);
+			if ($data['status'] == 'success') {
+				return "/admin/user";
+			}
+		}
+		print_r(__FILE__);
+		print_r($data);
+		die();
+	}
 }
