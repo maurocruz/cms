a547739422717537e14278c327c48c6be3142b6c
Mauro Cruz <maurocruz@pirenopolis.tur.br>
2022-12-17 15:18:09 -0300

    Change id name property

--- src/Request/Server/Type/OrderServer.php
+++ src/Request/Server/Type/OrderServer.php
@@ -9,52 +9,52 @@ use Plinct\Cms\Request\Server\ServerAbstract;
 
 class OrderServer
 {
-    /**
-     * @param $params
-     * @return string
-     */
-    public function new($params): string
-    {
-        $seller = $params['seller'];
-        $sellerType = $params['sellerType'];
-        // insert new order
-        $data = CmsFactory::request()->api()->post('order', $params)->ready();
-        $id = $data['id'];
-        // REGISTER HISTORY IN ORDER REFERENCE
-        $history = new HistoryServer('order', $id);
-        $history->setSummary("Created new order");
-        $history->register("CREATED");
-        // RESPONSE
-        return "/admin/" . lcfirst($sellerType) . "/order?id=$seller&item=$id";
-    }
+  /**
+   * @param $params
+   * @return string
+   */
+  public function new($params): string
+  {
+    $seller = $params['seller'];
+    $sellerType = $params['sellerType'];
+    // insert new order
+    $data = CmsFactory::request()->api()->post('order', $params)->ready();
+    $id = $data['idorder'];
+    // REGISTER HISTORY IN ORDER REFERENCE
+    $history = new HistoryServer('order', $id);
+    $history->setSummary("Created new order");
+    $history->register("CREATED");
+    // RESPONSE
+    return "/admin/" . lcfirst($sellerType) . "/order?id=$seller&item=$id";
+  }
 
-    /**
-     * @param $params
-     * @return mixed|void
-     */
-    public function edit($params)
-    {
-      // HISTORY
-      $dataOld = CmsFactory::request()->api()->get('order', [ "id" => $params['id'] ]);
-	    $history = new HistoryServer('order', $params['id']);
-	    $history->setSummaryByDifference($params, $dataOld[0]);
-	    $history->register("UPDATE")->ready();
-      // RESPONSE
-      return $params;
-    }
+  /**
+   * @param $params
+   * @return mixed|void
+   */
+  public function edit($params)
+  {
+    // HISTORY
+    $dataOld = CmsFactory::request()->api()->get('order', [ "idorder" => $params['idorder'] ])->ready();
+    $history = new HistoryServer('order', $params['idorder']);
+    $history->setSummaryByDifference($params, $dataOld[0]);
+    $history->register("UPDATE")->ready();
+    // RESPONSE
+    return $params;
+  }
 
-    /**
-     * @param $params
-     * @return string
-     */
-    public function erase($params): string
-    {
-        $idorder = $params['id'] ?? $params['idIsPartOf'] ?? $params['idorder'];
-        CmsFactory::request()->api()->delete('order', [ "idorder" => $idorder ])->ready();
+  /**
+   * @param $params
+   * @return string
+   */
+  public function erase($params): string
+  {
+    $idorder = $params['id'] ?? $params['idIsPartOf'] ?? $params['idorder'];
+    CmsFactory::request()->api()->delete('order', [ "idorder" => $idorder ])->ready();
 
-        $tableHasPart = $params['tableHasPart'] ?? lcfirst($params['sellerType']);
-        $idHasPart = $params['idHasPart'] ?? $params['seller'];
+    $tableHasPart = $params['tableHasPart'] ?? lcfirst($params['sellerType']);
+    $idHasPart = $params['idHasPart'] ?? $params['seller'];
 
-        return "/admin/$tableHasPart/order?id=$idHasPart";
-    }
+    return "/admin/$tableHasPart/order?id=$idHasPart";
+  }
 }
