fb3c9c3aedf9d2622df694f8c23edfbd518214cf
Mauro Cruz <maurocruz@pirenopolis.tur.br>
2024-12-09 17:25:23 -0300

    stylized button

--- src/View/Fragment/Button/Buttons.php
+++ src/View/Fragment/Button/Buttons.php
@@ -1,5 +1,4 @@
 <?php
-declare(strict_types=1);
 namespace Plinct\Cms\View\Fragment\Button;
 
 use Plinct\Cms\CmsFactory;
@@ -16,14 +15,14 @@ class Buttons
    */
   public function buttonDelete(string $idIsPartOf, string $tableISPartOf, string $idHasPart = null, string $tableHasPart = null, array $attributes = null): array
   {
-    $form = CmsFactory::response()->fragment()->form($attributes);
+    $form = CmsFactory::view()->fragment()->form($attributes);
     $form->action("/admin/$tableISPartOf/erase")->method('post');
     $form->input('idIsPartOf', $idIsPartOf, 'hidden')
       ->input('tableIsPartOf', $tableISPartOf, 'hidden')
       ->input('idHasPart', $idHasPart, 'hidden')
       ->input('tableHasPart', $tableHasPart, 'hidden');
-    $form->content("<button type='submit' class='button-submit' onclick='return confirm(\"" . _("Do you really want to delete this item?") . "\")'>"
-        . CmsFactory::response()->fragment()->icon()->delete()
+    $form->content("<button type='submit' class='button-submit form-submit-button-delete' onclick='return confirm(\"" . _("Do you really want to delete this item?") . "\")'>"
+        . CmsFactory::view()->fragment()->icon()->delete()
       ."</button>");
     return $form->ready();
   }
